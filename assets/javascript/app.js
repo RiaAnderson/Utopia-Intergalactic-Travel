d0ff8b97636c389b1806d9989859b27fc85af2e8
Prasamsha Sharma <prasamshari@gmail.com>
2019-09-14 12:01:37 -0700

    adding HTML

---
+++ assets/javascript/app.js
@@ -0,0 +1,29 @@
+// dlLv4z6e3rCisYRRncMENDhBax0qLP0LwKdbTm6X
+// api key nasa
+var url = "https://api.nasa.gov/planetary/apod?api_key=dlLv4z6e3rCisYRRncMENDhBax0qLP0LwKdbTm6X";
+
+
+$.ajax({
+  url: url,
+  success: function(result){
+  if("copyright" in result) {
+    $("#copyright").text("Image Credits: " + result.copyright);
+  }
+  else {
+    $("#copyright").text("Image Credits: " + "Public Domain");
+  }
+  
+  if(result.media_type == "video") {
+    $("#apod_img_id").css("display", "none"); 
+    $("#apod_vid_id").attr("src", result.url);
+  }
+  else {
+    $("#apod_vid_id").css("display", "none"); 
+    $("#apod_img_id").attr("src", result.url);
+  }
+  $("#reqObject").text(url);
+  $("#returnObject").text(JSON.stringify(result, null, 4));  
+  $("#apod_explaination").text(result.explanation);
+  $("#apod_title").text(result.title);
+}
+});
